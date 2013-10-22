b1549fd4c4839f6f1b582be1666d78c9c358b1d9
Cam Saül <cameron@getluckybird.com>
2013-10-22 14:31:35 -0700

    Add global property macros

---
+++ ExpaPlatform/XGlobalMacros.h
@@ -0,0 +1,17 @@
+//
+//  XGlobalMacros.h
+//  ExpaPlatform
+//
+//  Created by Cameron Saul on 10/22/13.
+//  Copyright (c) 2013 Expa, LLC. All rights reserved.
+//
+
+#define PROP			@property (nonatomic, readwrite)
+#define PROP_RO			@property (nonatomic, readonly)
+#define PROP_STRONG		@property (nonatomic, strong, readwrite)
+#define PROP_STRONG_RO	@property (nonatomic, strong, readonly)
+#define PROP_COPY		@property (nonatomic, copy, readwrite)
+#define PROP_COPY_RO	@property (nonatomic, copy, readonly)
+#define PROP_WEAK		@property (nonatomic, weak, readwrite)
+#define PROP_WEAK_RO	@property (nonatomic, weak, readonly)
+#define PROP_DELEGATE(PROTOCOL) PROP_WEAK id<PROTOCOL> delegate
