5a4b9d792384aba51aa8c9e86491f8426952df6b
Cam Saul <cam@getluckybird.com>
2013-12-19 15:27:41 -0800

    Remove stupid view lifecycle assertions, Spark Inspector is swizzling my methods and making my life hard

--- ExpaPlatform/Components/NavigationService/XViewController.m
+++ ExpaPlatform/Components/NavigationService/XViewController.m
@@ -8,19 +8,6 @@
 
 #import "XViewController.h"
 
-/// Check and make sure various calls in the view lifcycle are properly forwarded to super by doing some checking here
-@interface XViewController()
-PROP BOOL viewDidLoadCalled;
-PROP BOOL viewWillAppearCalled;
-PROP BOOL viewDidAppearCalled;
-PROP BOOL viewControllerWillBecomeActiveCalled;
-PROP BOOL viewControllerDidBecomeActiveCalled;
-PROP BOOL viewControllerWillBecomeInactiveCalled;
-PROP BOOL viewControllerDidBecomeInactiveCalled;
-PROP BOOL viewWillDisappearCalled;
-PROP BOOL viewDidDisappearCalled;
-@end
-
 @implementation XViewController
 
 + (void)validateParams:(NSDictionary *)params {}; // default implementation does nothing
@@ -51,55 +38,17 @@ PROP BOOL viewDidDisappearCalled;
 - (void)setup {}
 
 - (void)dealloc {
-	// only check if we're being presented by a GeoTipNavigationController (soon-to-be XNavigationController)
-	if (self.viewControllerWillBecomeInactiveCalled) {
-		NSAssert(self.viewControllerDidBecomeInactiveCalled, @"[super viewControllerDidBecomeInactive:] never reached the XViewController. More than likely, you're missing that call in a subclass. Make sure all subclasses call super!");
-	}
-	NSAssert(self.viewDidDisappearCalled, @"[super viewDidDisappear:] never reached the XViewController. More than likely, you're missing that call in a subclass. Make sure all subclasses call super!");
-	
 	[NSNotificationCenter.defaultCenter removeObserver:self];
-}
-	
-- (void)viewWillAppear:(BOOL)animated {
-	[super viewWillAppear:animated];
-	self.viewWillAppearCalled = YES;
 }
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	self.edgesForExtendedLayout = UIRectEdgeNone;
-	self.viewDidLoadCalled = YES;
-}
-
-- (void)viewDidAppear:(BOOL)animated {
-	NSAssert(self.viewDidLoadCalled, @"[super viewDidLoad:] never reached the XViewController. More than likely, you're missing that call in a subclass. Make sure all subclasses call super!");
-	NSAssert(self.viewWillAppearCalled, @"[super viewWillAppear:] never reached the XViewController. More than likely, you're missing that call in a subclass. Make sure all subclasses call super!");
-    [super viewDidAppear:animated];
-	
-	self.viewDidAppearCalled = YES;
-}
-
-- (void)viewWillDisappear:(BOOL)animated {
-	NSAssert(self.viewDidAppearCalled, @"[super viewDidAppear:] never reached the XViewController. More than likely, you're missing that call in a subclass. Make sure all subclasses call super!");
-	[super viewWillDisappear:animated];
-	self.viewWillDisappearCalled = YES;
-}
-
-- (void)viewDidDisappear:(BOOL)animated {
-	NSAssert(self.viewWillDisappearCalled, @"[super viewWillDisappear:] never reached the XViewController. More than likely, you're missing that call in a subclass. Make sure all subclasses call super!");
-	[super viewDidDisappear:animated];
-	self.viewDidDisappearCalled = YES;
 }
 
 //----- These methods don't exist as part of expa-ios per se (GeoTipNavigationController calls them) YET -- they will soon -----//
 
-- (void)viewControllerWillBecomeActive:(BOOL)animated {
-	self.viewControllerWillBecomeActiveCalled = YES;
-}
-
 - (void)viewControllerDidBecomeActive:(BOOL)animated {
-	NSAssert(self.viewControllerWillBecomeActiveCalled, @"[super viewControllerWillBecomeActive:] never reached the XViewController. More than likely, you're missing that call in a subclass. Make sure all subclasses call super!");
-	
 	// Since GTTracker isn't part of ExpaPlatform (yet) use ObjC runtime hackery to find the IMP and call it TODO.
 	const Class trackingClass = NSClassFromString(@"GTTracker");
 	#pragma clang diagnostic push
@@ -109,19 +58,12 @@ PROP BOOL viewDidDisappearCalled;
 	const IMP _trackingImp = [trackingClass methodForSelector:trackingSel];
 	void(*trackingImp)(id, SEL, UIViewController *) = (void(*)(id, SEL, UIViewController *))_trackingImp; // cast IMP to a c function so ARC doesn't try to release it
 	trackingImp(trackingClass, trackingSel, self);
-	
-	self.viewControllerDidBecomeActiveCalled = YES;
 }
 
-- (void)viewControllerWillBecomeInactive:(BOOL)animated {
-	NSAssert(self.viewControllerDidBecomeActiveCalled, @"[super viewControllerDidBecomeActive:] never reached the XViewController. More than likely, you're missing that call in a subclass. Make sure all subclasses call super!");
-	self.viewControllerWillBecomeInactiveCalled = YES;
-}
-
-- (void)viewControllerDidBecomeInactive:(BOOL)animated {
-	NSAssert(self.viewControllerWillBecomeInactiveCalled, @"[super viewControllerWillBecomeInactive:] never reached the XViewController. More than likely, you're missing that call in a subclass. Make sure all subclasses call super!");
-	self.viewControllerDidBecomeInactiveCalled = YES;
-}
+// default implementations of these do nothing
+- (void)viewControllerWillBecomeActive:(BOOL)animated {}
+- (void)viewControllerWillBecomeInactive:(BOOL)animated {}
+- (void)viewControllerDidBecomeInactive:(BOOL)animated {}
 
 //----- END GeoTip-only methods -----//
 
