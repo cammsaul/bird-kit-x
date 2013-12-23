5fa0c0a0870b6b710514f73258ab589bcc1b111b
Cam Saul <cam@getluckybird.com>
2013-12-22 17:45:18 -0800

    Add hooks so XLogging automatically logs to Crashlytics

--- ExpaPlatform/Components/Logging/XLogging.m
+++ ExpaPlatform/Components/Logging/XLogging.m
@@ -7,6 +7,10 @@
 //
 
 #import "XLogging.h"
+#import <objc/runtime.h>
+
+/// Support for Crashlytics
+OBJC_EXTERN void CLSLog(NSString *format, ...) NS_FORMAT_FUNCTION(1,2);
 
 static int CurrentLogLevel = LogLevelInfo;
 int *XLogLevel = &CurrentLogLevel;
@@ -21,21 +25,20 @@ char const * const StringForLogFlag(LogFlag flag) {
 	else return NULL;
 }
 
-#if DEBUG
-	void XLog(id sender, LogFlag flag, NSString *formatString, ...) {
-		if (!(CurrentLogLevel & flag)) return;
-		if (XLogClasses && ![XLogClasses containsObject:[sender class]]) return;
-		
-		va_list argptr;
-		va_start(argptr, formatString);
-		NSString *string = [[NSString alloc] initWithFormat:(NSString *)formatString arguments:argptr];
-		printf("[%s %s] %s\n",
-			   [NSStringFromClass([sender class]) cStringUsingEncoding:NSUTF8StringEncoding],
-			   StringForLogFlag(flag),
-			   [string cStringUsingEncoding:NSUTF8StringEncoding]);
-		va_end(argptr);
-	}
-#else
-	// empty inline function, hopefully compiler will optimize out this entire call
-	inline void XLog(id sender, LogFlag flag, NSString *formatString, ...) {}
-#endif
+void XLog(id sender, LogFlag flag, NSString *formatString, ...) {
+	if (!(CurrentLogLevel & flag)) return;
+	if (XLogClasses && ![XLogClasses containsObject:[sender class]]) return;
+	
+	va_list argptr;
+	va_start(argptr, formatString);
+	NSString *logMessage = [[NSString alloc] initWithFormat:(NSString *)formatString arguments:argptr];
+	va_end(argptr);
+
+	logMessage = [NSString stringWithFormat:@"[%s %s] %@", class_getName([sender class]), StringForLogFlag(flag), logMessage];
+
+	#if DEBUG
+		printf("%s\n", [logMessage cStringUsingEncoding:NSUTF8StringEncoding]);
+	#endif
+	
+	CLSLog(@"%@", logMessage);
+}
