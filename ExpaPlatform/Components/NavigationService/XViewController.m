29c155a90802cb70670fa9080daa9da08da6c1ec
Cam Saul <cam@geotip.com>
2014-02-14 19:34:20 -0800

    Prevent multiple calls to setup in XViewController

--- ExpaPlatform/Components/NavigationService/XViewController.m
+++ ExpaPlatform/Components/NavigationService/XViewController.m
@@ -6,20 +6,25 @@
 //  Copyright (c) 2013 Series G. All rights reserved.
 //
 
+#import <objc/runtime.h>
+
 #import "XViewController.h"
 #import "NSDictionary+Expa.h"
-
+#import "XRuntimeUtilities.h"
 @implementation XViewController
 
 + (void)validateParams:(NSDictionary *)params {}; // default implementation does nothing
 
+/// We don't want to accidentally call setup multiple times is various subclasses do/don't implement different versions of init, etc
+ASSOC_PROP_STRONG(NSNumber *, hasCalledSetup, setHasCalledSetup);
+
 - (instancetype)init {
-	if (self = [super init]) [self setup];
+	if (self = [super init]) [self callSetup];
 	return self;
 }
 
 - (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-	if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) [self setup];
+	if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) [self callSetup];
 	return self;
 }
 
@@ -32,12 +37,19 @@
                 _params = [_params dictionaryBySettingValue:nil forKey:XNavigationServiceDelegateParam];
             }
         }
-		[self setup];
+		[self callSetup];
 	}
 	return self;
 }
 
 - (void)awakeFromNib {
+	[self callSetup];
+}
+
+/// Helper to check self.hasCalledSetup so we don't do it more than once
+- (void)callSetup {
+	if (self.hasCalledSetup) return; // already done
+	self.hasCalledSetup = @YES;
 	[self setup];
 }
 
