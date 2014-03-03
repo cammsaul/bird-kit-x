86a904a9bd665f89dd6a9381f5cc77b759573a27
Cam Saul <cam@getluckybird.com>
2014-03-02 20:15:40 -0800

    XSingleton generalization of singleton pattern

--- ExpaPlatform/Components/XSingleton.m
+++ ExpaPlatform/Components/XSingleton.m
@@ -6,26 +6,28 @@
 //  Copyright (c) 2014 Expa, LLC. All rights reserved.
 //
 
+#import <objc/runtime.h>
+
 #import "XSingleton.h"
+#import "XRuntimeUtilities.h"
 
 @implementation XSingleton
 
+ASSOC_CLASS_PROP_STRONG(XSingleton *, singletonInstance, setSingletonInstance)
+
 + (instancetype)alloc {
-	@synchronized(self) {
-		static id __sharedInstance = nil;
-		NSAssert(!__sharedInstance, @"Attempt to allocate a second instance of singleton class %@!", NSStringFromClass([self class]));
-		__sharedInstance = [super alloc];
-		return __sharedInstance;
+	@synchronized([self class]) {
+		NSAssert(![self singletonInstance], @"Attempt to allocate a second instance of singleton class %@!", NSStringFromClass([self class]));
+		return [super alloc];
 	}
 }
 
 + (instancetype)sharedInstance {
-	@synchronized(self) {
-		static id __sharedInstance;
-		if (!__sharedInstance) {
-			__sharedInstance = [[[self class] alloc] init];
+	@synchronized([self class]) {
+		if (![self singletonInstance]) {
+			[self setSingletonInstance:[[[self class] alloc] init]];
 		}
-		return __sharedInstance;
+		return [self singletonInstance];
 	}
 }
 
